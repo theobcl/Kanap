fb243d3c03c89d7bc56f352dac1e18b25b01aead
Théophile Bancal <theophilebancal@MacBook-Pro-de-Theophile.local>
2022-10-06 19:29:08 +0200

    product.js page methods improved

--- front/js/product.js
+++ front/js/product.js
@@ -1,12 +1,5 @@
-// Recupérer l'ID du produit
+// Récupérer les éléments commums aux fonctions de ce fichier
 let itemId = new URL(document.location).searchParams.get("id");
-
-// Récuperer / Créer les éléments à générer sur la page produit
-let itemImgDiv = document.querySelector(".item__img");
-let itemImg = document.createElement("img");
-let itemName = document.querySelector("#title");
-let itemDescription = document.querySelector("#description");
-let itemPrice = document.querySelector("#price");
 let colorSelect = document.querySelector("#colors");
 
 main(); 
@@ -17,89 +10,79 @@ function main() {
 }; 
 
 function getItem() {
-  // Récupèrer uniquement les informations du produit de la page grace à son id
   fetch(`http://localhost:3000/api/products/${itemId}`)
-    .then(function (response) {
-    return response.json();
-    })
-    // Générer un message en cas d'erreur du fetching
-    .catch((error) => {
-    let container = document.querySelector("#item");
-    container.innerHTML = "Nous n'avons pas réussi à afficher notre produit. Avez vous bien lancé le serveur local (Port 3000) ?";
-    container.style.textAlign = "center";
-    container.style.padding = "50px 0";
+    // Récupérer uniquement les informations du produit de la page grace à son id
+    .then(function(response) {
+      if (response.ok) {
+          return response.json();
+      }
     })
     // Générer le contenu des différents éléments de la page
-    .then(function (dataAPI) {
-    item = dataAPI;
-
-    // Modifier le titre de l'onglet de la page sur le navigateur
-    document.title = item.name
-
-    // Générer l'image du produit
-    itemImgDiv.appendChild(itemImg);
-    itemImg.src = item.imageUrl;
-    
-    // Générer le nom du produit
-    itemName.innerHTML = item.name;
-
-    // Générer le prix du produit
-    itemPrice.innerHTML = `${item.price},00 `;
-
-    // Générer la description du produit
-    itemDescription.innerText = item.description;
-
-    // Générer les options de couleurs du produit
-    for (let i = 0; i < item.colors.length; i++) {
-        let option = document.createElement("option");
-        option.innerText = item.colors[i];
-        colorSelect.appendChild(option);
-    }
-  });
-}
-
+    .then(function(item) {
+      // Modifier le titre de l'onglet de la page sur le navigateur
+      document.title = item.name
+      // Générer l'image du produit
+      let itemImgDiv = document.querySelector(".item__img");
+      let itemImg = document.createElement("img");
+      itemImgDiv.appendChild(itemImg);
+      itemImg.src = item.imageUrl;
+      // Générer le nom du produit
+      document.querySelector("#title").innerHTML = item.name;
+      // Générer le prix du produit
+      document.querySelector("#price").innerHTML = `${item.price},00 `;
+      // Générer la description du produit
+      document.querySelector("#description").innerText = item.description;
+      // Générer les options de couleurs du produit
+      for (let i = 0; i < item.colors.length; i++) {
+          let option = document.createElement("option");
+          option.innerText = item.colors[i];
+          colorSelect.appendChild(option);
+      }
+    })
+    // Générer un message en cas d'erreur
+    .catch(function(error) {
+      console.log(error)
+    });
+};
 
 function addToCart() {
-  // Récuperer la couleur sélectionnée par l'utilisateur
-  let itemColor = colorSelect.value
-  // Récuperer la quantité sélectionnée par l'utilisateur
-  let itemQuantity = document.querySelector("#quantity");
   // Récuperer le bouton d'ajout au panier
   let addToCartBtn = document.querySelector("#addToCart");
-  
   // Création d'un evenement lors du click sur le bouton d'ajout au panier
   addToCartBtn.addEventListener("click", () => {
+    // Récuperer la couleur sélectionnée par l'utilisateur
+    let itemColor = colorSelect.value
+    // Récuperer la quantité sélectionnée par l'utilisateur
+    let itemQuantity = document.querySelector("#quantity").value;
     // Si 0 < quantité < 100 et qu'une couleur a été choisi
-    if (itemQuantity.value > 0 && itemQuantity.value < 100 && itemColor !== "--SVP, choisissez une couleur --") {
-
+    if (0 < itemQuantity < 100 && itemColor !== "--SVP, choisissez une couleur --") {
       // Création du objet avec les informations du produit ajouté au panier
       let itemAdded = {
-        _id: itemId,
+        id: itemId,
         color: itemColor,
-        quantity: parseFloat(itemQuantity.value),
+        quantity: parseFloat(itemQuantity),
       };
 
-      //  Gestion du localStorage
-      let productsInCart = [];
-      
-      // Si le LS existe, on insère son contenu dans productsInCart, puis on le renvoit vers le LS avec le nouveau produit ajouté.
-      if (localStorage.getItem("items") !== null) {
-        productsInCart = JSON.parse(localStorage.getItem("items"));
-      } 
-      // Si le LS est vide, on le crée avec le produit ajouté
-      productsInCart.push(itemAdded);
-      localStorage.setItem("items", JSON.stringify(productsInCart));
-
-      // Message de confirmation lors d'un ajout au panier
-      itemContentContainer = document.querySelector(".item__content");
-      
-      if (typeof confirmationMessage == 'undefined') {
-        confirmationMessage = document.createElement("h4")
-        itemContentContainer.appendChild(confirmationMessage);
-        confirmationMessage.innerHTML = `Vous avez ajouté ${itemQuantity.value} canapé(s) à votre panier !`;
-        confirmationMessage.justifyContent = "center";
-        setTimeout("location.reload(true);", 4000);
+      let productInKart = JSON.parse(localStorage.getItem("item"))
+      // s'il y a un produit dans le local storage //
+      if (productInKart) {
+        // On check si ce produit n'est pas le même que celui qu'on ajoute au panier
+        const similarItem = productInKart.find(element => element.id == itemAdded.id && element.color == itemAdded.color);
+        if (similarItem) {
+          // Si c'est le cas, on incremente la quantité du produit
+          similarItem.quantity += itemAdded.quantity;
+          localStorage.setItem("item", JSON.stringify(productInKart));
+        } else {
+          // Sinon, on ajoute un nouveau produit
+          productInKart.push(itemAdded);
+          localStorage.setItem("item", JSON.stringify(productInKart));
+        }
+      // Si le LS n'existe pas encore, on l'initialise et on ajoute le produit au panier
+      } else {
+        productInKart = []
+        productInKart.push(itemAdded)
+        localStorage.setItem("item", JSON.stringify(productInKart))
       }
     };
   });
-}
+};
--- front/js/script.js
+++ front/js/script.js
@@ -1,25 +1,31 @@
-function getAllProducts () {
-  fetch("http://localhost:3000/api/products")
-  .then(function(res) {
-      if (res.ok) {
-          return res.json();
-      }
-  })
-  .then(function(value) {  
-      document.getElementById("items").innerHTML = value.map(item => `
-          <a href="./product.html?id=${item._id}">
-              <article>
-                  <img src=${item.imageUrl} alt=${item.altTxt}>
-                  <h3 class="productName">${item.name}</h3>
-                  <p class="productDescription">${item.description}</p>
-              </article>
-          </a>
-      `).join('')
-  })
-  .catch(function(err) {
-      console.log(err)
-      // Une erreur est survenue
-  });
-}
+main(); 
 
-getAllProducts()
+function main() {
+  getAllItems();
+}; 
+
+function getAllItems () {
+  fetch("http://localhost:3000/api/products")
+    // Récupérer les données de tous les items dans l'API
+    .then(function(response) {
+      if (response.ok) {
+          return response.json();
+      }
+    })
+    // Générer un nouvel élément HTML pour chaque item de l'API
+    .then(function(value) {  
+      document.getElementById("items").innerHTML = value.map(item => `
+        <a href="./product.html?id=${item._id}">
+          <article>
+            <img src=${item.imageUrl} alt=${item.altTxt}>
+            <h3 class="productName">${item.name}</h3>
+            <p class="productDescription">${item.description}</p>
+          </article>
+        </a>
+      `).join('')
+    })
+    // Générer un message en cas d'erreur
+    .catch(function(error) {
+      console.log(error)
+    })
+};
